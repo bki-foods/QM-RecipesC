3a156ec7c5e345edf54f802fef87aa6d68d0516f
Nmobki <nmo@bki.dk>
2020-06-18 13:04:42 +0200

    Update QM_Recipes_C.py

--- QM_Recipes_C.py
+++ QM_Recipes_C.py
@@ -13,24 +13,61 @@ import datetime
 
 
 # Define server connection and SQL query:
-server = '192.168.125.161'
-db = 'BKI_IMP_EXP'
-con = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + db + ';UID=' + user + ';PWD=' + password)
-query = """ SELECT
-            	DATEADD(d,DATEDIFF(d,0,[RECORDING_DATE]),0) AS [Date]
-            	,[ROASTER]
-                ,[CUSTOMER_CODE] AS [Recipe]
-                ,AVG([FINAL_TEMP_ROASTING] / 10.0) AS [End temp]
-            FROM [dbo].[PRO_EXP_BATCH_DATA_ROASTER]
-			WHERE DATEADD(d,DATEDIFF(d,0,[RECORDING_DATE]),0) > DATEADD(month,DATEDIFF(month,0,GETDATE())-6,0)
-                 AND [CUSTOMER_CODE] IN ('10401005','10401523','10401009','10401057','10401207','10401522','10401028','10401087','10401054','10401510')
-                 AND [ROASTER] = 'R2'
+server = 'sqlsrv04\tx'
+db = 'TXprodSTAGE'
+con = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + db)
+query = """ WITH PBOM AS (
+            SELECT
+            	[Top level item]
+            	,[Level]
+            	,[Component qty per]
+            	,[Component]
+            	,[Active version]
+            FROM [dbo].[ProductionBOM version by level_V]
+            WHERE [Active version] = 1
+            	AND [Component] LIKE '1040%'
+            	AND [Top level item] > '20000000'
+            )
+            , VE AS (
+            SELECT
+            	[Item No.]
+            	,COUNT([Entry No.]) AS [Count]
+            	,SUM([AntalKgKaffe]) AS [AntalKgKaffe]
+            	,SUM([Sales Amount (Actual)]) AS [Sales Amount (Actual)]
+            	,SUM([Cost Amount (Actual) korr. export]) AS [Cost Amount (Actual) korr. export]
+            FROM [NAV].[Value Entry_V]
+            WHERE
+            	[Item Ledger Entry Type] IN (-1,1)
+            	AND [Posting Date] >= DATEADD(year, -1, getdate())
             GROUP BY
-            	DATEADD(d,DATEDIFF(d,0,[RECORDING_DATE]),0)
-            	,[ROASTER]
-                ,[CUSTOMER_CODE]
-			HAVING COUNT(*) >= 3
-            ORDER BY
-                DATEADD(d,DATEDIFF(d,0,[RECORDING_DATE]),0) ASC
-                ,[CUSTOMER_CODE] ASC           	
-                ,[ROASTER] ASC"""
+            	[Item No.]
+            )
+            
+            SELECT
+            	I.[No.]
+            	,SUM(ISNULL(VE.[Count],0)) AS [Count]
+            	,SUM(ISNULL(CASE
+            		WHEN PBOM.[Level] = 0
+            			THEN ( PBOM.[Component qty per] / ITOP.[Net Weight] ) * VE.[AntalKgKaffe]
+            		ELSE [Component qty per] * VE.[AntalKgKaffe]
+            	END,0)) AS [Kg kaffe]
+            	,SUM(ISNULL(CASE
+            		WHEN PBOM.[Level] = 0
+            			THEN ( PBOM.[Component qty per] / ITOP.[Net Weight] ) * VE.[Sales Amount (Actual)]
+            		ELSE [Component qty per] * VE.[Sales Amount (Actual)]
+            	END,0)) AS [Sales amount]
+            	,SUM(ISNULL(CASE
+            		WHEN PBOM.[Level] = 0
+            			THEN ( PBOM.[Component qty per] / ITOP.[Net Weight] ) * VE.[Cost Amount (Actual) korr. export]
+            		ELSE [Component qty per] * VE.[Cost Amount (Actual) korr. export]
+            	END,0)) AS [Cost amount]
+            FROM [NAV].[Item_V] AS I
+            LEFT JOIN PBOM
+            	ON I.[No.] = PBOM.[Component]
+            LEFT JOIN [NAV].[Item_V] AS ITOP
+            	ON PBOM.[Top level item] = ITOP.[No.]
+            LEFT JOIN  VE
+            	ON PBOM.[Top level item] = VE.[Item No.]
+            WHERE I.[No.] LIKE '1040%'
+            GROUP BY
+            	I.[No.] """
