fd821f9533f680e1aa6cb3215f84ffb4543c7b24
Nmobki <nmo@bki.dk>
2020-06-18 14:34:16 +0200

    Update QM_Recipes_C.py

--- QM_Recipes_C.py
+++ QM_Recipes_C.py
@@ -11,9 +11,8 @@ import datetime
 # Read data from SQL datasource into dataframe
 # =============================================================================
 
-
 # Define server connection and SQL query:
-server = 'sqlsrv04\tx'
+server = r'sqlsrv04\tx'
 db = 'TXprodSTAGE'
 con = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + db)
 query = """ WITH PBOM AS (
@@ -44,8 +43,9 @@ query = """ WITH PBOM AS (
             )
             
             SELECT
-            	I.[No.] AS [Recipe]
+            	I.[No.] AS [ItemNo]
             	,SUM(ISNULL(VE.[Count],0)) AS [Count]
+				,DATEDIFF(d,I.[Oprettet den],getdate()) AS [Days]
             	,SUM(ISNULL(CASE
             		WHEN PBOM.[Level] = 0
             			THEN ( PBOM.[Component qty per] / ITOP.[Net Weight] ) * VE.[AntalKgKaffe]
@@ -70,26 +70,29 @@ query = """ WITH PBOM AS (
             	ON PBOM.[Top level item] = VE.[Item No.]
             WHERE I.[No.] LIKE '1040%'
             GROUP BY
-            	I.[No.] """
+            	I.[No.]
+				,I.[Oprettet den] """
 
 # =============================================================================
 # Read query and other variables
 # =============================================================================
 # change env variable below to switch between dev and prod SQL tables for inserts
 env = 'dev'             # dev = test || cp = prod
-# Read query into dataframe and create unique list for iteration and empty dataframes:
-df = pd.read_sql(query, con)
-df['MonetaryValue'] = df['Amount'] - df['Cost']
-recipes = df.Recipe.unique()
-dfQuan = pd.DataFrame()
-dfCons = pd.Dataframe()
 # Variables for inserting data into sql database:
 params = urllib.parse.quote_plus('DRIVER={SQL Server Native Client 10.0};SERVER=sqlsrv04;DATABASE=BKI_Datastore;Trusted_Connection=yes')
 engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
 # Other variables:
 now = datetime.datetime.now()
-script_name = 'QM_Recipes_C.py'
 execution_id = int(now.timestamp())
+script_name = 'QM_Recipes_C.py'
+seg_type = 'Coffee Recipes'
+cols_sale = ['ExecutionId', 'Timestamp', 'ItemNo', 'Quantity', 'MonetaryValue', 'Score', 'Type', 'Script']
+cols_no_sale = ['ExecutionId', 'Timestamp', 'ItemNo', 'Score', 'Type', 'Script']
+cols_quan = (['ExecutionId', 'Timestamp', 'Type', 'Quantile', 'Quantity',
+             'MonetaryValue'])
+# Read query into dataframe and create unique list for iteration and empty dataframes:
+df = pd.read_sql(query, con)
+df['MonetaryValue'] = df['Amount'] - df['Cost']
 
 
 # =============================================================================
@@ -112,9 +115,41 @@ def insert_sql(dataframe, table_name, schema):
     dataframe.to_sql(table_name, con=engine, schema=schema, if_exists='append', index=False)
     
 
+# =============================================================================
+# SKUs with sales
+# =============================================================================
 
+# Create dataframe and skip if it's empty:
+df_sales = df.loc[df['Count'] != 0]
+if len(df_sales) != 0:
+    # Define quantiles
+    quantiles = df_sales.quantile(q=[0.25, 0.5, 0.75]).to_dict()
+    # Identify quartiles per measure for each product:
+    df_sales.loc[:, 'QuantityQuartile'] = df_sales['Quantity'].apply(qm_score, args=('Quantity', quantiles,))
+    df_sales.loc[:, 'MonetaryQuartile'] = df_sales['MonetaryValue'].apply(qm_score, args=('MonetaryValue', quantiles,))
+    # Concetenate Quartile measurements to single string:
+    df_sales.loc[:, 'Score'] = df_sales.QuantityQuartile * 10 + df_sales.MonetaryQuartile
+    # Create data stamps for dataframe:
+    df_sales.loc[:, 'Timestamp'] = now
+    df_sales.loc[:, 'Type'] = seg_type
+    df_sales.loc[:, 'ExecutionId'] = execution_id
+    df_sales.loc[:, 'Script'] = script_name
+    # Insert dataframe to SQL database:
+    insert_sql(df_sales[cols_sale], 'ItemSegmentation', env)
+    # Append quantiles to dataframe:
+    df_quan = pd.DataFrame.from_dict(quantiles)
+    df_quan.loc[:, 'Type'] =  seg_type
+    df_quan.loc[:, 'Timestamp'] = now
+    df_quan.loc[:, 'ExecutionId'] = execution_id
+    df_quan.loc[:, 'Quantile'] = df_quan.index
+    # Insert quantiles to SQL database:
+    insert_sql(df_quan[cols_quan], 'ItemSegmentationQuantiles', env)
 
+# =============================================================================
+# SKUs without sales
+# =============================================================================
 
-
-
+# Create dataframe and skip if it's empty:
+df_no_sales = df.loc[df['Count'] == 0]
+if len(df_no_sales) != 0:
 
